f30c2cc6f0f25e1246f3ba6f04c34876ce0c98e5
datacat01 <olha.pashnieva@gmail.com>
2020-09-06 23:12:53 +0300

    random coord for line and rect

--- draw_on_opencv_video.py
+++ draw_on_opencv_video.py
@@ -1,5 +1,7 @@
 import cv2
+import numpy as np
 import math
+import random
 
 """
     Встановити OpenCV, зчитати зображення з вебки, відобразити в першому віконці
@@ -11,18 +13,23 @@ import math
     кроків замість звалища картинок матимемо відеофайл (наприклад .avi).
 """
 
-VIDEO_PATH_1 = 'initial_output.mp4'
+VIDEO_PATH_1 = 'initial_video.mp4'
 VIDEO_PATH_2 = 'result.mp4'
 FOURCC = cv2.VideoWriter_fourcc(*'mp4v')
+FPS = 10.0
 
 get_height = lambda capture: math.ceil(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 get_width = lambda capture: math.ceil(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+rand_color = lambda: tuple(np.random.choice(range(256), 3).tolist())
+
+
+def rand_coord(max_width, max_height, st=0): 
+    return (random.randint(st, max_width), random.randint(st, max_height))
 
 
 def capture_wideocam():
     cap = cv2.VideoCapture(0)
-
-    out = cv2.VideoWriter(VIDEO_PATH_1, FOURCC, 10.0, (get_width(cap), get_height(cap)))
+    out = cv2.VideoWriter(VIDEO_PATH_1, FOURCC, FPS, (get_width(cap), get_height(cap)))
 
     while(cap.isOpened()):
         ret, frame = cap.read() # If frame is read correctly, ret will be True.
@@ -42,18 +49,27 @@ def capture_wideocam():
 
 def draw_rectangle(path=VIDEO_PATH_1):
     cap = cv2.VideoCapture(path)
-    out = cv2.VideoWriter(VIDEO_PATH_2, FOURCC, 10.0, (get_width(cap), get_height(cap)))
+    frame_width, frame_height = get_width(cap), get_height(cap)
+    out = cv2.VideoWriter(VIDEO_PATH_2, FOURCC, FPS, (frame_width, frame_height))
+
+    colors = [rand_color() for i in range(2)]
+    line_coord = [rand_coord(frame_width, frame_height) for i in range(2)]
+    rect_coord = [rand_coord(frame_width//2, frame_height//2)]
+    rect_coord.append(
+        (rect_coord[0][0] + random.randint(0, frame_width//2),
+        rect_coord[0][1] + random.randint(0, frame_height//2))
+        )
 
     while(cap.isOpened()):
         ret, frame = cap.read()
         gray_one_channel = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray_three_channels = cv2.cvtColor(gray_one_channel, cv2.COLOR_GRAY2BGR)
 
-        cv2.line(gray_three_channels,(0,0),(150,150),(255,255,255),15)
-        cv2.rectangle(gray_three_channels,(15,25),(200,150),(0,0,255),15)
+        cv2.line(gray_three_channels, line_coord[0], line_coord[1], colors[0], 5)
+        cv2.rectangle(gray_three_channels, rect_coord[0], rect_coord[1], colors[1], 5)
 
-        cv2.imshow('video result', gray_three_channels)
-        cv2.imshow('video original', frame)
+        cv2.imshow('video result (q to exit)', gray_three_channels)
+        cv2.imshow('video original (q to exit)', frame)
 
         out.write(gray_three_channels)
         
